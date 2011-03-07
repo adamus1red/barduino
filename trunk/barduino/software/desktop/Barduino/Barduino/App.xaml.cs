72a0e0418396779a0ed6dbbdabe4f5c062b6bca0
Knoxie89 <Knoxie89@5a18de63-cd80-314a-860d-1d88bb3fd365>
2011-03-07 02:45:14 +0000

    Fixed Page layout to fit King Tophers likings and Added some panels

--- trunk/barduino/software/desktop/Barduino/Barduino/App.xaml.cs
+++ trunk/barduino/software/desktop/Barduino/Barduino/App.xaml.cs
@@ -12,8 +12,5 @@ namespace Barduino
     /// </summary>
     public partial class App : Application
     {
-
-        Ingredient Rum = new Ingredient(Ingredient.LiquidType.Soda, "CocaCola", "Coke", 8);
-
     }
 }
